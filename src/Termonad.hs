4cdc8ffbcd4a414fa1cecadda04a4c0c629e1476
(cdep)illabout <cdep.illabout@gmail.com>
2018-03-19 00:13:35 +0900

    Updated library versions.

--- src/Termonad.hs
+++ src/Termonad.hs
@@ -1,8 +1,31 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedLabels #-}
 
 module Termonad where
 
-import Termonad.Prelude
+import Termonad.Prelude hiding (on)
+
+import GI.Gtk
+import qualified GI.Gtk as Gtk
+-- import Data.GI.Base (AttrOp((:=)), new, on, set)
+-- import Data.GI.Base
 
 defaultMain :: IO ()
-defaultMain = pure ()
+defaultMain = do
+  Gtk.init Nothing
+  win <- new Gtk.Window [ #title := "Hi there" ]
+  on win #destroy Gtk.mainQuit
+  button <- new Gtk.Button [ #label := "Click me" ]
+  on
+    button
+    #clicked
+      ( set
+          button
+          [ #sensitive := False
+          , #label := "Thanks for clicking me"
+          ]
+      )
+  #add win button
+  #showAll win
+  Gtk.main
