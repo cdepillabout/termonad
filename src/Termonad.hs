aac741965ce2f04a2acacd782ee929a5b2780c9a
(cdep)illabout <cdep.illabout@gmail.com>
2018-03-21 22:26:03 +0900

    Make the terminal actually start.

--- src/Termonad.hs
+++ src/Termonad.hs
@@ -6,6 +6,8 @@ module Termonad where
 
 import Termonad.Prelude hiding (on)
 
+import GI.Gio
+import GI.GLib.Flags (SpawnFlags(..))
 import GI.Gtk
 import qualified GI.Gtk as Gtk
 import GI.Vte
@@ -15,17 +17,30 @@ defaultMain = do
   void $ Gtk.init Nothing
   win <- new Window [#title := "Hi there"]
   void $ on win #destroy mainQuit
+
+  box <- new Box [#orientation := OrientationVertical]
+
   button <- new Button [#label := "Click me"]
   void $
     on
       button
       #clicked
       (set button [#sensitive := False, #label := "Thanks for clicking me"])
-  #add win button
+  boxPackStart box button False False 0
 
-  -- terminalNew
   term <- new Terminal []
-  #add win term
+  termResVal <-
+    terminalSpawnSync
+      term
+      [PtyFlagsDefault]
+      Nothing
+      ["/usr/bin/env", "bash"]
+      Nothing
+      [SpawnFlagsDefault]
+      Nothing
+      (Nothing :: Maybe Cancellable)
+  boxPackStart box term True True 0
 
+  #add win box
   #showAll win
   Gtk.main
