b1adf2c2901e938d5d0c98012f121679cbc0e64f
(cdep)illabout <cdep.illabout@gmail.com>
2018-04-03 23:29:13 +0900

    Stop using the appStartup function.

--- src/Termonad.hs
+++ src/Termonad.hs
@@ -524,20 +524,7 @@ showAboutDialog app = do
   #present aboutDialog
 
 appStartup :: Application -> IO ()
-appStartup app = do
-  aboutAction <- simpleActionNew "about" Nothing
-  void $ Gdk.on aboutAction #activate (const $ showAboutDialog app)
-  #addAction app aboutAction
-
-  quitAction <- simpleActionNew "quit" Nothing
-  void $ Gdk.on quitAction #activate (\_ -> putStrLn "got quit!")
-  #addAction app quitAction
-  #setAccelsForAction app "app.quit" ["<Shift><Ctrl>Q"]
-
-  menuBuilder <- builderNewFromString menuText $ fromIntegral (length menuText)
-  menuModel <- objFromBuildUnsafe menuBuilder "menubar" MenuModel
-  -- applicationSetAppMenu app (Just menuModel)
-  #setMenubar app (Just menuModel)
+appStartup _app = putStrLn "called appStartup"
 
 defaultMain :: IO ()
 defaultMain = do
