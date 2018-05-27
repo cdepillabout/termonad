d5ac043fb35b261a44bd8a3586ca041a2e21fc71
(cdep)illabout <cdep.illabout@gmail.com>
2018-05-27 23:52:39 +0900

    Start rewriting the focus list api.

--- src/Termonad/FocusedList.hs
+++ src/Termonad/FocusedList.hs
@@ -1,9 +1,12 @@
-{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Termonad.FocusedList where
 
 import Termonad.Prelude
 
+import Control.Lens
+import Control.Lens.TH
 import Data.Constraint ((:-)(Sub), Dict(Dict))
 import Data.Constraint.Nat (plusNat)
 import Data.Finite (Finite, finite, getFinite, shift, weaken)
@@ -12,102 +15,97 @@ import Data.Void
 import GHC.TypeLits (type (+), type (<=), type (<=?), KnownNat, Nat, natVal)
 import Unsafe.Coerce (unsafeCoerce)
 
-
--- TODO: Reimplement this stuff without using KnownNat.
+newtype Focus = Focus
+  { unFocus :: Int
+  } deriving newtype (Eq, Num, Read, Show)
 
 -- TODO: Probably be better
 -- implemented as an Order statistic tree
 -- (https://en.wikipedia.org/wiki/Order_statistic_tree).
-data FocusedList :: Nat -> * -> * where
-  FocusedListEmpty :: FocusedList 0 a
-  FocusedList :: Finite n -> IntMap a -> FocusedList n a
+data FocusedList a = FocusedList
+  { focusedListFocus :: {-# UNPACK #-} !Focus
+  , focusedListLen :: {-# UNPACK #-} !Int
+  , focusedList :: !(IntMap a)
+  } deriving (Read, Show)
 
-singletonFL :: a -> FocusedList 1 a
-singletonFL a = FocusedList (finite 0) (singletonMap 0 a)
+$(makeLensesFor
+    [ ("focusedListFocus", "focusedListFocusLens")
+    , ("focusedListLen", "focusedListLenLens")
+    , ("focusedList", "focusedListLens")
+    ]
+    ''FocusedList
+ )
 
-appendFL :: forall n a. KnownNat n => a -> FocusedList n a -> FocusedList (n + 1) a
-appendFL a FocusedListEmpty = singletonFL a
-appendFL a (FocusedList fin intmap) =
-  case plusNat @n @1 of
-    Sub Dict ->
-      FocusedList (weaken fin) (insertMap (natValInt @n) a intmap)
+focusedListAtLens :: Int -> Lens' (FocusedList a) (Maybe a)
+focusedListAtLens i = focusedListLens . at i
 
--- | This will return a 'FocusedList' with an empty element at index @m@.
-unsafeShiftUpFrom ::
-     forall proxy m n a. (KnownNat m, KnownNat n, m <= n, 1 <= n)
-  => proxy m
-  -> FocusedList n a
-  -> FocusedList (n + 1) a
-unsafeShiftUpFrom _ (FocusedList fin intmap) =
-  let newFin =
-        if getFiniteInt fin < natValInt @m
-          then weaken fin
-          else shift fin
-      newMap = unsafeShiftMapUp (natValInt @n - 1) (natValInt @m - 1) intmap
-  in
-  case plusNat @n @1 of
-    Sub Dict -> FocusedList newFin newMap
+singletonFL :: a -> FocusedList a
+singletonFL a =
+  FocusedList
+    { focusedListFocus = 0
+    , focusedListLen = 1
+    , focusedList = singletonMap 0 a
+    }
 
-unsafeLookup :: Int -> IntMap a -> a
-unsafeLookup i intmap =
-  case lookup i intmap of
-    Nothing -> error $ "unsafeLookup: key " <> show i <> " not found in intmap"
-    Just a -> a
+appendFL :: a -> FocusedList a -> FocusedList a
+appendFL a fl =
+  fl &
+    focusedListLenLens +~ 1 &
+    focusedListAtLens (fl ^. focusedListLenLens) .~ Just a
 
-unsafeShiftMapUp :: Int -> Int -> IntMap a -> IntMap a
-unsafeShiftMapUp start end intmap
-  | start > end =
-      let val = unsafeLookup start intmap
-          newMap = insertMap (start + 1) val (deleteMap start intmap)
-      in unsafeShiftMapUp (start - 1) end newMap
-  | otherwise = intmap
+insertFL :: Int -> FocusedList a -> Maybe (FocusedList a)
+insertFL i fl = undefined
 
-insertFL ::
-     forall proxy m n a. (KnownNat m, KnownNat n, m <= n)
-  => proxy m
-  -> a
-  -> FocusedList n a
-  -> FocusedList (n + 1) a
-insertFL _ a FocusedListEmpty = singletonFL a
-insertFL proxyM a focusedList@(FocusedList fin _) =
-  case lala fin of
-    Refl ->
-      case unsafeShiftUpFrom proxyM focusedList of
-        FocusedListEmpty -> absurd $ gaga (Proxy @n)
-        FocusedList newFin intmap ->
-          let newMap = insertMap (natValInt @m) a intmap
-          in
-          case plusNat @n @1 of
-            Sub Dict -> FocusedList newFin newMap
+-- -- | This will return a 'FocusedList' with an empty element at index @m@.
+-- unsafeShiftUpFrom ::
+--      forall proxy m n a. (KnownNat m, KnownNat n, m <= n, 1 <= n)
+--   => proxy m
+--   -> FocusedList n a
+--   -> FocusedList (n + 1) a
+-- unsafeShiftUpFrom _ (FocusedList fin intmap) =
+--   let newFin =
+--         if getFiniteInt fin < natValInt @m
+--           then weaken fin
+--           else shift fin
+--       newMap = unsafeShiftMapUp (natValInt @n - 1) (natValInt @m - 1) intmap
+--   in
+--   case plusNat @n @1 of
+--     Sub Dict -> FocusedList newFin newMap
 
-papa :: Int
-papa =
-  let a = singletonFL ("hello" :: Text)
-      b = insertFL (Proxy @1) "bye" a
-      c = insertFL (Proxy @2) "good" b
-      d = insertFL (Proxy @0) "yo" c
-  in
-  case d of
-    FocusedList _ _ -> 3
+-- unsafeLookup :: Int -> IntMap a -> a
+-- unsafeLookup i intmap =
+--   case lookup i intmap of
+--     Nothing -> error $ "unsafeLookup: key " <> show i <> " not found in intmap"
+--     Just a -> a
 
-dada :: (1 <= n) => FocusedList n a -> Int
-dada (FocusedList _ _) = 3
+-- unsafeShiftMapUp :: Int -> Int -> IntMap a -> IntMap a
+-- unsafeShiftMapUp start end intmap
+--   | start > end =
+--       let val = unsafeLookup start intmap
+--           newMap = insertMap (start + 1) val (deleteMap start intmap)
+--       in unsafeShiftMapUp (start - 1) end newMap
+--   | otherwise = intmap
 
-hoho :: (KnownNat n, 1 <= n, IsString a) => FocusedList n a -> Int
-hoho fl =
-  let a = insertFL (Proxy @0) "bye" fl
-  in
-  case a of
-    FocusedList _ _ -> 10
+-- insertFL ::
+--      forall proxy m n a. (KnownNat m, KnownNat n, m <= n)
+--   => proxy m
+--   -> a
+--   -> FocusedList n a
+--   -> FocusedList (n + 1) a
+-- insertFL _ a FocusedListEmpty = singletonFL a
+-- insertFL proxyM a focusedList@(FocusedList fin _) =
+--   case lala fin of
+--     Refl ->
+--       case unsafeShiftUpFrom proxyM focusedList of
+--         FocusedListEmpty -> absurd $ gaga (Proxy @n)
+--         FocusedList newFin intmap ->
+--           let newMap = insertMap (natValInt @m) a intmap
+--           in
+--           case plusNat @n @1 of
+--             Sub Dict -> FocusedList newFin newMap
 
-gaga :: forall n proxy. (KnownNat n, (n + 1) ~ 0) => proxy n -> Void
-gaga _ = error "void"
+-- natValInt :: forall m. KnownNat m => Int
+-- natValInt = fromIntegral $ natVal (Proxy @m)
 
-lala :: KnownNat x => Finite x -> (1 <=? x) :~: 'True
-lala _ = unsafeCoerce Refl
-
-natValInt :: forall m. KnownNat m => Int
-natValInt = fromIntegral $ natVal (Proxy @m)
-
-getFiniteInt :: Finite n -> Int
-getFiniteInt = fromIntegral . getFinite
+-- getFiniteInt :: Finite n -> Int
+-- getFiniteInt = fromIntegral . getFinite
