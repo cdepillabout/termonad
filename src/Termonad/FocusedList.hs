76c2c4d8cdaea42576c57f30e359e344806bef8c
(cdep)illabout <cdep.illabout@gmail.com>
2018-05-31 10:18:45 +0900

    Add insertFL function.

--- src/Termonad/FocusedList.hs
+++ src/Termonad/FocusedList.hs
@@ -29,15 +29,15 @@ data FocusList a = FocusList
   } deriving (Read, Show)
 
 $(makeLensesFor
-    [ ("focusListFocus", "focusListFocusLens")
-    , ("focusListLen", "focusListLenLens")
-    , ("focusList", "focusListLens")
+    [ ("focusListFocus", "lensFocusListFocus")
+    , ("focusListLen", "lensFocusListLen")
+    , ("focusList", "lensFocusList")
     ]
     ''FocusList
  )
 
-focusListAtLens :: Int -> Lens' (FocusList a) (Maybe a)
-focusListAtLens i = focusListLens . at i
+lensFocusListAt :: Int -> Lens' (FocusList a) (Maybe a)
+lensFocusListAt i = lensFocusList . at i
 
 singletonFL :: a -> FocusList a
 singletonFL a =
@@ -47,14 +47,69 @@ singletonFL a =
     , focusList = singletonMap 0 a
     }
 
-appendFL :: a -> FocusList a -> FocusList a
-appendFL a fl =
-  fl &
-    focusListLenLens +~ 1 &
-    focusListAtLens (fl ^. focusListLenLens) ?~ a
+-- | This is an invariant that the 'FocusList' must always protect.
+invariantFL :: FocusList a -> Bool
+invariantFL fl = False
 
-insertFL :: Int -> FocusList a -> Maybe (FocusList a)
-insertFL i fl = undefined
+-- | Append a new value to the end of a 'FocusList'.
+appendFL :: a -> FocusList a -> FocusList a
+appendFL a fl = unsafeInsertNewFL (fl ^. lensFocusListLen) a fl
+
+-- | Unsafely insert a new @a@ in a 'FocusList'.  This sets the 'Int' value to
+-- @a@.  The length of the 'FocusList' will be increased by 1.  The
+-- 'FocusList's 'Focus' is not changed.
+--
+-- If there is some value in the 'FocusList' already at the 'Int', then it will
+-- be overwritten.  Also, the 'Int' is not checked to make sure it is above 0.
+unsafeInsertNewFL :: Int -> a -> FocusList a -> FocusList a
+unsafeInsertNewFL i a fl =
+  fl &
+    lensFocusListLen +~ 1 &
+    lensFocusListAt i ?~ a
+
+-- | This unsafely shifts all values up in a 'FocusList'.  It also updates the
+-- 'Focus' of the 'FocusList' if it has been shifted.
+--
+-- It does not check that the 'Int' is greater than 0.
+unsafeShiftUpFrom :: forall a. Show a => Int -> FocusList a -> FocusList a
+unsafeShiftUpFrom i fl =
+  let intMap = fl ^. lensFocusList
+      lastElemIdx = (fl ^. lensFocusListLen) - 1
+      newIntMap = go i lastElemIdx intMap
+      oldFocus = fl ^. lensFocusListFocus
+  in
+  fl &
+    lensFocusList .~ newIntMap &
+    lensFocusListFocus .~ if i > lastElemIdx then oldFocus else oldFocus + 1
+  where
+    go :: Show a => Int -> Int -> IntMap a -> IntMap a
+    go idxToInsert idxToShiftUp intMap
+      | idxToInsert <= idxToShiftUp =
+        let val = unsafeLookup idxToShiftUp intMap
+            newMap =
+              insertMap (idxToShiftUp + 1) val (deleteMap idxToShiftUp intMap)
+        in go idxToInsert (idxToShiftUp - 1) newMap
+      | otherwise = intMap
+
+-- | This is an unsafe lookup function.  This assumes that the 'Int' exists in
+-- the 'IntMap'.
+unsafeLookup :: Int -> IntMap a -> a
+unsafeLookup i intmap =
+  case lookup i intmap of
+    Nothing -> error $ "unsafeLookup: key " <> show i <> " not found in intmap"
+    Just a -> a
+
+insertFL :: Show a => Int -> a -> FocusList a -> Maybe (FocusList a)
+insertFL i a fl =
+  if i < 0 || i > (fl ^. lensFocusListLen)
+    then
+      -- Return Nothing if the insertion position is out of bounds.
+      Nothing
+    else
+      -- Otherwise, shift all existing values up one and insert the new
+      -- value in the opened place.
+      let shiftedUpFL = unsafeShiftUpFrom i fl
+      in Just $ unsafeInsertNewFL i a shiftedUpFL
 
 -- -- | This will return a 'FocusList' with an empty element at index @m@.
 -- unsafeShiftUpFrom ::
@@ -72,11 +127,6 @@ insertFL i fl = undefined
 --   case plusNat @n @1 of
 --     Sub Dict -> FocusList newFin newMap
 
--- unsafeLookup :: Int -> IntMap a -> a
--- unsafeLookup i intmap =
---   case lookup i intmap of
---     Nothing -> error $ "unsafeLookup: key " <> show i <> " not found in intmap"
---     Just a -> a
 
 -- unsafeShiftMapUp :: Int -> Int -> IntMap a -> IntMap a
 -- unsafeShiftMapUp start end intmap
