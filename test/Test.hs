b96531c3c4af256f7183a14fd1a975e1f8f4563c
Dennis Gosnell <cdep.illabout@gmail.com>
2018-07-08 13:18:54 +0900

    Add a command for removing a value from a focus list.

--- test/Test.hs
+++ test/Test.hs
@@ -5,7 +5,7 @@ import Termonad.Prelude hiding (assert)
 
 import Control.Lens ((^.))
 import Hedgehog
-  ( Callback(Ensure, Update)
+  ( Callback(Ensure, Require, Update)
   , Command(Command)
   , Concrete(Concrete)
   , HTraversable(htraverse)
@@ -28,7 +28,16 @@ import Hedgehog.Range (linear)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.Hedgehog (testProperty)
 
-import Termonad.FocusList (FocusList, emptyFL, insertFL, invariantFL, lensFocusListLen, lookupFL)
+import Termonad.FocusList
+  ( FocusList
+  , emptyFL
+  , insertFL
+  , invariantFL
+  , isEmptyFL
+  , lensFocusListLen
+  , lookupFL
+  , removeFL
+  )
 
 main :: IO ()
 main = do
@@ -51,7 +60,9 @@ foo =
         sequential
           (linear 1 100)
           initialState
-          [ insertFLCommand ]
+          [ insertFLCommand
+          , removeFLCommand
+          ]
     executeSequential initialState actions
 
 -- property :: HasCallStack => PropertyT IO () -> Property
@@ -85,10 +96,24 @@ foo =
 --         [Callback input output state]
 --     }
 
+-----------
+-- State --
+-----------
+
 newtype State a (v :: * -> *) = State { unState :: FocusList (Var a v) } deriving (Eq, Show)
 
 initialState :: State a v
 initialState = State emptyFL
+
+ensureInvariants :: State String Concrete -> State String Concrete -> a -> b -> Test ()
+ensureInvariants (State startingFL) (State endingFL) _ _ = do
+  assert (invariantFL startingFL)
+  assert (invariantFL endingFL)
+
+
+--------------
+-- InsertFL --
+--------------
 
 data InsertFL a v = InsertFL !(FocusList (Var a v)) !Int !a deriving (Eq, Show)
 
@@ -104,7 +129,8 @@ insertFLCommand =
   Command
     generator
     execute
-    [ Update update
+    [ Require require
+    , Update update
     , Ensure ensureInvariants
     , Ensure ensureStringInFL
     ]
@@ -125,14 +151,16 @@ insertFLCommand =
           failure
         Just _ -> pure newVal
 
+    require :: State String Symbolic -> InsertFL String v -> Bool
+    require (State fl) (InsertFL _ newKey _) =
+      let len = fl ^. lensFocusListLen
+      in newKey < len
+
     update :: forall v. State String v -> InsertFL String v -> Var String v -> State String v
     update (State fl) (InsertFL _ newKey _) newVal =
       case insertFL newKey newVal fl of
         Nothing -> error "insertFLCommand, update: Failed to insert a value into the FocusList, even though we should be able to"
         Just newFL -> State newFL
-
-    ensureInvariants :: State String Concrete -> State String Concrete -> InsertFL String Concrete -> String -> Test ()
-    ensureInvariants _ (State endingFL) _ _ = assert (invariantFL endingFL)
 
     ensureStringInFL :: State String Concrete -> State String Concrete -> InsertFL String Concrete -> String -> Test ()
     ensureStringInFL _ (State endingFL) (InsertFL _ newKey newVal) _ =
@@ -144,6 +172,62 @@ insertFLCommand =
           failure
         Just val -> val === Var (Concrete newVal)
 
+--------------
+-- RemoveFL --
+--------------
+
+data RemoveFL a v = RemoveFL !(FocusList (Var a v)) !Int deriving (Eq, Show)
+
+instance HTraversable (RemoveFL x) where
+  htraverse :: forall f g h. Applicative f => (forall a. g a -> f (h a)) -> RemoveFL x g -> f (RemoveFL x h)
+  htraverse func (RemoveFL fl keyToRemove) = RemoveFL <$> traverse go fl <*> pure keyToRemove
+    where
+      go :: forall a. Var a g -> f (Var a h)
+      go var = htraverse func var
+
+removeFLCommand :: forall n m. (MonadGen n, MonadTest m) => Command n m (State String)
+removeFLCommand =
+  Command
+    generator
+    execute
+    [ Require require
+    , Update update
+    , Ensure ensureInvariants
+    , Ensure ensureActuallyRemoved
+    ]
+  where
+    generator :: State String Symbolic -> Maybe (n (RemoveFL String Symbolic))
+    generator (State fl) =
+      Just $ do
+        let len = fl ^. lensFocusListLen
+        keyToRemove <- int $ linear 0 len
+        pure (RemoveFL fl keyToRemove)
+
+    execute :: RemoveFL String Concrete -> m ()
+    execute (RemoveFL fl keyToRemove) =
+      case removeFL keyToRemove fl of
+        Nothing -> annotate "Can't remove from an empty focus list"
+        Just _ -> pure ()
+
+    require :: State String Symbolic -> RemoveFL String v -> Bool
+    require (State fl) (RemoveFL _ keyToRemove) =
+      let len = fl ^. lensFocusListLen
+      in isEmptyFL fl || keyToRemove < len
+
+    update :: forall v. State String v -> RemoveFL String v -> Var () v -> State String v
+    update (State fl) (RemoveFL _ keyToRemove) _ =
+      case removeFL keyToRemove fl of
+        Nothing -> State fl
+        Just newFL -> State newFL
+
+    ensureActuallyRemoved :: State String Concrete -> State String Concrete -> RemoveFL String Concrete -> () -> Test ()
+    ensureActuallyRemoved (State startingFL) (State endingFL) (RemoveFL _ _) () =
+      if isEmptyFL startingFL
+        then startingFL === endingFL
+        else
+          let startingFLLen = startingFL ^. lensFocusListLen
+              endingFLLen = endingFL ^. lensFocusListLen
+          in startingFLLen - 1 === endingFLLen
 
 -- data Callback input output state =
 --   -- | A pre-condition for a command that must be verified before the command
