94352e81ea329c2612fd63cd4a06ab56225f19e7
Dennis Gosnell <cdep.illabout@gmail.com>
2018-07-09 13:17:27 +0900

    Start working on my new testing thing.

--- test/Test.hs
+++ test/Test.hs
@@ -9,10 +9,12 @@ import Hedgehog
   ( Callback(Ensure, Require, Update)
   , Command(Command)
   , Concrete(Concrete)
+  , Gen
   , HTraversable(htraverse)
   , MonadGen
   , MonadTest
   , Property
+  , PropertyT
   , Symbolic
   , Test
   , Var(Var)
@@ -25,9 +27,10 @@ import Hedgehog
   , failure
   , forAll
   , property
+  , success
   , withShrinks
   )
-import Hedgehog.Gen (ascii, int, sequential, string)
+import Hedgehog.Gen (ascii, choice, int, sequential, string)
 import Hedgehog.Range (constant, linear)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.Hedgehog (testProperty)
@@ -56,231 +59,280 @@ testsIO = do
   pure $
     testGroup
       "tests"
-      [ testProperty "foo" foo
+      [ testProperty "bar" bar
       ]
 
-foo :: Property
-foo =
+bar :: Property
+bar =
   property $ do
-    actions <-
-      forAll $ do
-        sequential
-          (linear 1 200)
-          initialState
-          [ insertFLCommand
-          , removeFLCommand
-          , deleteFLCommand
-          ]
-    traceShowM actions
-    executeSequential initialState actions
+    numOfActions <- forAll $ int (linear 1 200)
+    let initialState = emtpyFL
+    runActions numOfActions emptyFL
 
------------
--- State --
------------
+data Action a
+  = InsertFL Int a
+  | RemoveFL Int
+  | DeleteFL a
+  deriving (Eq, Show)
 
-newtype State a (v :: * -> *) = State { unState :: FocusList (Var a v) } deriving (Eq, Show)
+genInsertFL :: Gen a -> FocusList a -> Maybe (Gen (Action a))
+genInsertFL valGen fl
+  | isEmptyFL fl = Just $ do
+    val <- valGen
+    pure $ InsertFL 0 val
+  | otherwise = Just $ do
+    let len = fl ^. lensFocusListLen
+    key <- int $ constant 0 len
+    val <- valGen
+    pure $ InsertFL key val
 
-initialState :: State a v
-initialState = State emptyFL
+genRemoveFL :: FocusList a -> Maybe (Gen (Action a))
+genRemoveFL fl = Nothing
 
-ensureInvariants :: State String Concrete -> State String Concrete -> a -> b -> Test ()
-ensureInvariants (State startingFL) (State endingFL) _ _ = do
-  assert (invariantFL startingFL)
-  annotate (debugFL endingFL)
-  assert (invariantFL endingFL)
+genDeleteFL :: Gen a -> FocusList a -> Maybe (Gen (Action a))
+genDeleteFL valGen fl = Nothing
 
---------------
--- InsertFL --
---------------
+generateAction :: Gen a -> FocusList a -> Gen (Action a)
+generateAction valGen fl = do
+  let generators = catMaybes [genInsertFL, genRemoveFL, genDeleteFL]
+  case generators of
+    [] ->
+      let msg =
+            "No generators available for fl:\n" <>
+            debugFL fl
+      in error msg
+    _ -> do
+      choice generators
 
-data InsertFL a v = InsertFL !Int !a deriving (Eq, Show)
+runActions :: Int -> FocusList a -> PropertyT m ()
+runActions i fl
+  | i <= 0 = success
+  | otherwise = do
+    action <- generateAction undefined fl
+    undefined
 
-instance HTraversable (InsertFL x) where
-  htraverse :: forall f g h. Applicative f => (forall a. g a -> f (h a)) -> InsertFL x g -> f (InsertFL x h)
-  htraverse _ (InsertFL newKey newVal) = pure (InsertFL newKey newVal)
+--foo :: Property
+--foo =
+--  property $ do
+--    actions <-
+--      forAll $ do
+--        sequential
+--          (linear 1 200)
+--          initialState
+--          [ insertFLCommand
+--          , removeFLCommand
+--          , deleteFLCommand
+--          ]
+--    traceShowM actions
+--    executeSequential initialState actions
 
-insertFLCommand :: forall n m. (MonadGen n, MonadTest m) => Command n m (State String)
-insertFLCommand =
-  Command
-    generator
-    execute
-    [ Update update
-    , Require require
-    , Ensure ensureInvariants
-    , Ensure ensureStringInFL
-    ]
-  where
-    generator :: State String Symbolic -> Maybe (n (InsertFL String Symbolic))
-    generator (State fl) =
-      Just $ do
-        let len = fl ^. lensFocusListLen
-        newKey <- int $ constant 0 len
-        newVal <- string (constant 0 25) ascii
-        pure (InsertFL newKey newVal)
+-------------
+---- State --
+-------------
 
-    execute :: InsertFL String Concrete -> m String
-    execute (InsertFL _ newVal) = pure newVal
+--newtype State a (v :: * -> *) = State { unState :: FocusList (Var a v) } deriving (Eq, Show)
 
-    require :: State String Symbolic -> InsertFL String Symbolic -> Bool
-    require (State fl) (InsertFL newKey _) =
-      let len = fl ^. lensFocusListLen
-      in
-      if len == 0
-        then newKey == 0
-        else newKey >= 0 && newKey <= len
+--initialState :: State a v
+--initialState = State emptyFL
 
-    update :: forall v. State String v -> InsertFL String v -> Var String v -> State String v
-    update (State fl) (InsertFL newKey newVal) newValVar =
-      case insertFL newKey newValVar fl of
-        Nothing ->
-          let msg =
-                "insertFLCommand, update: Failed to insert a value into " <>
-                "the FocusList, even though we should be able to" <>
-                  "\nnewKey: " <>
-                  show newKey <>
-                  "\nnewVal: " <>
-                  show newVal <>
-                  "\nfocus list len: " <>
-                  show (fl ^. lensFocusListLen)
-          in error msg
-        Just newFL -> State newFL
+--ensureInvariants :: State String Concrete -> State String Concrete -> a -> b -> Test ()
+--ensureInvariants (State startingFL) (State endingFL) _ _ = do
+--  assert (invariantFL startingFL)
+--  annotate (debugFL endingFL)
+--  assert (invariantFL endingFL)
 
-    ensureStringInFL :: State String Concrete -> State String Concrete -> InsertFL String Concrete -> String -> Test ()
-    ensureStringInFL _ (State endingFL) (InsertFL newKey newVal) _ =
-      let maybeVal = lookupFL newKey endingFL
-      in
-      case maybeVal of
-        Nothing -> do
-          annotate (debugFL endingFL)
-          annotateShow newKey
-          annotateShow newVal
-          annotate "Couldn't find inserted value in FocusList"
-          failure
-        Just val -> val === Var (Concrete newVal)
+----------------
+---- InsertFL --
+----------------
 
---------------
--- RemoveFL --
---------------
+--data InsertFL a v = InsertFL !Int !a deriving (Eq, Show)
 
-newtype RemoveFL a v = RemoveFL Int deriving (Eq, Show)
+--instance HTraversable (InsertFL x) where
+--  htraverse :: forall f g h. Applicative f => (forall a. g a -> f (h a)) -> InsertFL x g -> f (InsertFL x h)
+--  htraverse _ (InsertFL newKey newVal) = pure (InsertFL newKey newVal)
 
-instance HTraversable (RemoveFL x) where
-  htraverse :: forall f g h. Applicative f => (forall a. g a -> f (h a)) -> RemoveFL x g -> f (RemoveFL x h)
-  htraverse func (RemoveFL keyToRemove) = pure $ RemoveFL keyToRemove
+--insertFLCommand :: forall n m. (MonadGen n, MonadTest m) => Command n m (State String)
+--insertFLCommand =
+--  Command
+--    generator
+--    execute
+--    [ Update update
+--    , Require require
+--    , Ensure ensureInvariants
+--    , Ensure ensureStringInFL
+--    ]
+--  where
+--    generator :: State String Symbolic -> Maybe (n (InsertFL String Symbolic))
+--    generator (State fl) =
+--      Just $ do
+--        let len = fl ^. lensFocusListLen
+--        newKey <- int $ constant 0 len
+--        newVal <- string (constant 0 25) ascii
+--        pure (InsertFL newKey newVal)
 
-removeFLCommand :: forall n m. (MonadGen n, MonadTest m) => Command n m (State String)
-removeFLCommand =
-  Command
-    generator
-    execute
-    [ Update update
-    , Require require
-    , Ensure ensureInvariants
-    , Ensure ensureActuallyRemoved
-    ]
-  where
-    generator :: State String Symbolic -> Maybe (n (RemoveFL String Symbolic))
-    generator (State fl) =
-      if isEmptyFL fl
-        then Nothing
-        else
-          Just $ do
-            let len = fl ^. lensFocusListLen
-            keyToRemove <- int $ constant 0 (len - 1)
-            pure $ RemoveFL keyToRemove
+--    execute :: InsertFL String Concrete -> m String
+--    execute (InsertFL _ newVal) = pure newVal
 
-    execute :: RemoveFL String Concrete -> m ()
-    execute _ = pure ()
+--    require :: State String Symbolic -> InsertFL String Symbolic -> Bool
+--    require (State fl) (InsertFL newKey _) =
+--      let len = fl ^. lensFocusListLen
+--      in
+--      if len == 0
+--        then newKey == 0
+--        else newKey >= 0 && newKey <= len
 
-    require :: State String Symbolic -> RemoveFL String v -> Bool
-    require (State fl) (RemoveFL keyToRemove) =
-      traceShow fl $ traceShow keyToRemove $
-      not (isEmptyFL fl) && keyToRemove < (fl ^. lensFocusListLen)
+--    update :: forall v. State String v -> InsertFL String v -> Var String v -> State String v
+--    update (State fl) (InsertFL newKey newVal) newValVar =
+--      case insertFL newKey newValVar fl of
+--        Nothing ->
+--          let msg =
+--                "insertFLCommand, update: Failed to insert a value into " <>
+--                "the FocusList, even though we should be able to" <>
+--                  "\nnewKey: " <>
+--                  show newKey <>
+--                  "\nnewVal: " <>
+--                  show newVal <>
+--                  "\nfocus list len: " <>
+--                  show (fl ^. lensFocusListLen)
+--          in error msg
+--        Just newFL -> State newFL
 
-    update :: forall a v. State String v -> RemoveFL String v -> Var a v -> State String v
-    update (State fl) (RemoveFL keyToRemove) _ =
-      case removeFL keyToRemove fl of
-        Nothing ->
-          let msg =
-                "removeFLCommand, update: Failed to remove a value from " <>
-                "the FocusList, even though we should be able to." <>
-                "\nkeyToRemove: " <>
-                show keyToRemove <>
-                "\nfocus list: " <>
-                debugFL (fmap (const "(can't show)") fl)
-          in error msg
-        Just newFL -> State newFL
+--    ensureStringInFL :: State String Concrete -> State String Concrete -> InsertFL String Concrete -> String -> Test ()
+--    ensureStringInFL _ (State endingFL) (InsertFL newKey newVal) _ =
+--      let maybeVal = lookupFL newKey endingFL
+--      in
+--      case maybeVal of
+--        Nothing -> do
+--          annotate (debugFL endingFL)
+--          annotateShow newKey
+--          annotateShow newVal
+--          annotate "Couldn't find inserted value in FocusList"
+--          failure
+--        Just val -> val === Var (Concrete newVal)
 
-    ensureActuallyRemoved :: State String Concrete -> State String Concrete -> RemoveFL String Concrete -> a -> Test ()
-    ensureActuallyRemoved (State startingFL) (State endingFL) (RemoveFL keyToRemove) _ = do
-      let startingFLLen = startingFL ^. lensFocusListLen
-          endingFLLen = endingFL ^. lensFocusListLen
-      annotateShow keyToRemove
-      annotate (debugFL startingFL)
-      annotate (debugFL endingFL)
-      startingFLLen - 1 === endingFLLen
+----------------
+---- RemoveFL --
+----------------
 
---------------
--- DeleteFL --
---------------
+--newtype RemoveFL a v = RemoveFL Int deriving (Eq, Show)
 
-data DeleteFL a v = DeleteFL !(Var a v) deriving (Eq, Show)
+--instance HTraversable (RemoveFL x) where
+--  htraverse :: forall f g h. Applicative f => (forall a. g a -> f (h a)) -> RemoveFL x g -> f (RemoveFL x h)
+--  htraverse func (RemoveFL keyToRemove) = pure $ RemoveFL keyToRemove
 
-instance HTraversable (DeleteFL x) where
-  htraverse :: forall f g h. Applicative f => (forall a. g a -> f (h a)) -> DeleteFL x g -> f (DeleteFL x h)
-  htraverse func (DeleteFL itemToRemove) = DeleteFL <$> htraverse func itemToRemove
+--removeFLCommand :: forall n m. (MonadGen n, MonadTest m) => Command n m (State String)
+--removeFLCommand =
+--  Command
+--    generator
+--    execute
+--    [ Update update
+--    , Require require
+--    , Ensure ensureInvariants
+--    , Ensure ensureActuallyRemoved
+--    ]
+--  where
+--    generator :: State String Symbolic -> Maybe (n (RemoveFL String Symbolic))
+--    generator (State fl) =
+--      if isEmptyFL fl
+--        then Nothing
+--        else
+--          Just $ do
+--            let len = fl ^. lensFocusListLen
+--            keyToRemove <- int $ constant 0 (len - 1)
+--            pure $ RemoveFL keyToRemove
 
-deleteFLCommand :: forall n m. (MonadGen n, MonadTest m) => Command n m (State String)
-deleteFLCommand =
-  Command
-    generator
-    execute
-    [ Update update
-    , Require require
-    , Ensure ensureInvariants
-    , Ensure ensureLenLess
-    , Ensure ensureActuallyRemoved
-    ]
-  where
-    generator :: State String Symbolic -> Maybe (n (DeleteFL String Symbolic))
-    generator (State fl) =
-      let len = fl ^. lensFocusListLen
-      in
-      if len == 0
-          then Nothing
-          else
-            Just $ do
-              keyToGetItem <- int $ constant 0 (len - 1)
-              let maybeItemToDelete = lookupFL keyToGetItem fl
-              case maybeItemToDelete of
-                Just itemToDelete -> pure (DeleteFL itemToDelete)
-                Nothing ->
-                  let msg =
-                        "Couldn't find item at index even though it should exist." <>
-                        "\nlen: " <>
-                        show len <>
-                        "\nkeyToGetItem: " <>
-                        show keyToGetItem
-                  in error msg
+--    execute :: RemoveFL String Concrete -> m ()
+--    execute _ = pure ()
 
-    execute :: DeleteFL String Concrete -> m ()
-    execute _ = pure ()
+--    require :: State String Symbolic -> RemoveFL String v -> Bool
+--    require (State fl) (RemoveFL keyToRemove) =
+--      traceShow fl $ traceShow keyToRemove $
+--      not (isEmptyFL fl) && keyToRemove < (fl ^. lensFocusListLen)
 
-    require :: State String Symbolic -> DeleteFL String Symbolic -> Bool
-    require (State fl) (DeleteFL itemToRemove) =
-      isJust (indexOfFL itemToRemove fl)
+--    update :: forall a v. State String v -> RemoveFL String v -> Var a v -> State String v
+--    update (State fl) (RemoveFL keyToRemove) _ =
+--      case removeFL keyToRemove fl of
+--        Nothing ->
+--          let msg =
+--                "removeFLCommand, update: Failed to remove a value from " <>
+--                "the FocusList, even though we should be able to." <>
+--                "\nkeyToRemove: " <>
+--                show keyToRemove <>
+--                "\nfocus list: " <>
+--                debugFL (fmap (const "(can't show)") fl)
+--          in error msg
+--        Just newFL -> State newFL
 
-    update :: forall v a. Eq1 v => State String v -> DeleteFL String v -> Var a v -> State String v
-    update (State fl) (DeleteFL itemToRemove) _ = State $ deleteFL itemToRemove fl
+--    ensureActuallyRemoved :: State String Concrete -> State String Concrete -> RemoveFL String Concrete -> a -> Test ()
+--    ensureActuallyRemoved (State startingFL) (State endingFL) (RemoveFL keyToRemove) _ = do
+--      let startingFLLen = startingFL ^. lensFocusListLen
+--          endingFLLen = endingFL ^. lensFocusListLen
+--      annotateShow keyToRemove
+--      annotate (debugFL startingFL)
+--      annotate (debugFL endingFL)
+--      startingFLLen - 1 === endingFLLen
 
-    ensureLenLess :: State String Concrete -> State String Concrete -> DeleteFL String Concrete -> a -> Test ()
-    ensureLenLess (State startingFL) (State endingFL) (DeleteFL itemToRemove) _ = do
-      let startingFLLen = startingFL ^. lensFocusListLen
-          endingFLLen = endingFL ^. lensFocusListLen
-      annotate (debugFL startingFL)
-      annotate (debugFL endingFL)
-      assert $ startingFLLen > endingFLLen
+----------------
+---- DeleteFL --
+----------------
 
-    ensureActuallyRemoved :: State String Concrete -> State String Concrete -> DeleteFL String Concrete -> a -> Test ()
-    ensureActuallyRemoved _ (State endingFL) (DeleteFL itemToRemove) _ =
-      indexOfFL itemToRemove endingFL === Nothing
+--data DeleteFL a v = DeleteFL !(Var a v) deriving (Eq, Show)
+
+--instance HTraversable (DeleteFL x) where
+--  htraverse :: forall f g h. Applicative f => (forall a. g a -> f (h a)) -> DeleteFL x g -> f (DeleteFL x h)
+--  htraverse func (DeleteFL itemToRemove) = DeleteFL <$> htraverse func itemToRemove
+
+--deleteFLCommand :: forall n m. (MonadGen n, MonadTest m) => Command n m (State String)
+--deleteFLCommand =
+--  Command
+--    generator
+--    execute
+--    [ Update update
+--    , Require require
+--    , Ensure ensureInvariants
+--    , Ensure ensureLenLess
+--    , Ensure ensureActuallyRemoved
+--    ]
+--  where
+--    generator :: State String Symbolic -> Maybe (n (DeleteFL String Symbolic))
+--    generator (State fl) =
+--      let len = fl ^. lensFocusListLen
+--      in
+--      if len == 0
+--          then Nothing
+--          else
+--            Just $ do
+--              keyToGetItem <- int $ constant 0 (len - 1)
+--              let maybeItemToDelete = lookupFL keyToGetItem fl
+--              case maybeItemToDelete of
+--                Just itemToDelete -> pure (DeleteFL itemToDelete)
+--                Nothing ->
+--                  let msg =
+--                        "Couldn't find item at index even though it should exist." <>
+--                        "\nlen: " <>
+--                        show len <>
+--                        "\nkeyToGetItem: " <>
+--                        show keyToGetItem
+--                  in error msg
+
+--    execute :: DeleteFL String Concrete -> m ()
+--    execute _ = pure ()
+
+--    require :: State String Symbolic -> DeleteFL String Symbolic -> Bool
+--    require (State fl) (DeleteFL itemToRemove) =
+--      isJust (indexOfFL itemToRemove fl)
+
+--    update :: forall v a. Eq1 v => State String v -> DeleteFL String v -> Var a v -> State String v
+--    update (State fl) (DeleteFL itemToRemove) _ = State $ deleteFL itemToRemove fl
+
+--    ensureLenLess :: State String Concrete -> State String Concrete -> DeleteFL String Concrete -> a -> Test ()
+--    ensureLenLess (State startingFL) (State endingFL) (DeleteFL itemToRemove) _ = do
+--      let startingFLLen = startingFL ^. lensFocusListLen
+--          endingFLLen = endingFL ^. lensFocusListLen
+--      annotate (debugFL startingFL)
+--      annotate (debugFL endingFL)
+--      assert $ startingFLLen > endingFLLen
+
+--    ensureActuallyRemoved :: State String Concrete -> State String Concrete -> DeleteFL String Concrete -> a -> Test ()
+--    ensureActuallyRemoved _ (State endingFL) (DeleteFL itemToRemove) _ =
+--      indexOfFL itemToRemove endingFL === Nothing
