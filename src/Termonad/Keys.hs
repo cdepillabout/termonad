5b1e6c0f24b8e7f07c15dcd0098542a16ac0e4ee
Dennis Gosnell <cdep.illabout@gmail.com>
2018-07-19 21:13:26 +0900

    Remove warnings from Termonad.Keys.

--- src/Termonad/Keys.hs
+++ src/Termonad/Keys.hs
@@ -4,13 +4,8 @@ module Termonad.Keys where
 import Termonad.Prelude
 
 import Control.Lens (imap)
-import Data.Default (def)
-import Data.Unique (Unique, newUnique)
-import qualified GI.Gdk as Gdk
 import GI.Gdk
-  ( AttrOp((:=))
-  , EventKey
-  , GObject
+  ( EventKey
   , pattern KEY_1
   , pattern KEY_2
   , pattern KEY_3
@@ -20,54 +15,12 @@ import GI.Gdk
   , pattern KEY_7
   , pattern KEY_8
   , pattern KEY_9
-  , pattern KEY_T
-  , ManagedPtr
   , ModifierType(..)
-  , castTo
   , get
-  , new
-  , screenGetDefault
   )
-import GI.Gio
-  ( ApplicationFlags(ApplicationFlagsFlagsNone)
-  , MenuModel(MenuModel)
-  , actionMapAddAction
-  , applicationRun
-  , noCancellable
-  , onSimpleActionActivate
-  , simpleActionNew
-  )
-import GI.GLib.Flags (SpawnFlags(..))
-import GI.Gtk
-  ( Application
-  , ApplicationWindow(ApplicationWindow)
-  , Box(Box)
-  , CssProvider(CssProvider)
-  , Dialog(Dialog)
-  , Notebook(Notebook)
-  , ScrolledWindow(ScrolledWindow)
-  , pattern STYLE_PROVIDER_PRIORITY_APPLICATION
-  , applicationNew
-  , applicationSetAccelsForAction
-  , builderNewFromString
-  , builderSetApplication
-  , noWidget
-  , styleContextAddProviderForScreen
-  )
-import qualified GI.Gtk as Gtk
-import GI.Pango
-  ( FontDescription
-  , pattern SCALE
-  , fontDescriptionNew
-  , fontDescriptionSetFamily
-  , fontDescriptionSetSize
-  )
-import GI.Vte (CursorBlinkMode(..), PtyFlags(..), Terminal(Terminal))
-import Text.XML (renderText)
-import Text.XML.QQ (Document, xmlRaw)
 
-import Termonad.Term
-import Termonad.Types
+import Termonad.Term (altNumSwitchTerm)
+import Termonad.Types (TMState)
 
 
 showKeys :: EventKey -> IO Bool
