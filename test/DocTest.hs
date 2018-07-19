3f14d6160cf0cba12e0c69680a03292cf9e3225c
Dennis Gosnell <cdep.illabout@gmail.com>
2018-07-19 21:24:57 +0900

    Clean up doctests a little.

--- test/DocTest.hs
+++ test/DocTest.hs
@@ -2,12 +2,11 @@
 module Main where
 
 import Build_doctests (flags, pkgs, module_sources)
-import Data.Foldable (traverse_)
 import Test.DocTest (doctest)
 
 main :: IO ()
 main = do
-  traverse_ putStrLn args
   doctest args
   where
+    args :: [String]
     args = flags ++ pkgs ++ module_sources
