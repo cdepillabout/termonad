8f0422fe0bdf046b41706b0bb1de5c12d0aae0bb
(cdep)illabout <cdep.illabout@gmail.com>
2018-07-21 13:00:02 +0900

    Get nix working with 17.09 and 18.03.

--- shell.nix
+++ shell.nix
@@ -1,8 +1,15 @@
 
 let
   nixpkgsTarball = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/dae9cf6106da19f79a39714f183ed253c62b32c5.tar.gz";
-    sha256 = "0r3c00m96ldb9z81ay7vj8gnpk4bf8gjcdiad7mgxvwxr9ndskjx";
+    # 17.09 (this works)
+    # url = "https://github.com/NixOS/nixpkgs/archive/39cd40f7bea40116ecb756d46a687bfd0d2e550e.tar.gz";
+    # sha256 = "0kpx4h9p1lhjbn1gsil111swa62hmjs9g93xmsavfiki910s73sh";
+    # 18.03 (this works)
+    url = "https://github.com/NixOS/nixpkgs/archive/120b013e0c082d58a5712cde0a7371ae8b25a601.tar.gz";
+    sha256 = "0hk4y2vkgm1qadpsm4b0q1vxq889jhxzjx3ragybrlwwg54mzp4f";
+    # recent version of nixpkgs (as of 2018-07-19)
+    # url = "https://github.com/NixOS/nixpkgs/archive/d7d31fea7e7eef8ff4495e75be5dcbb37fb215d0.tar.gz"
+    # sha256 = "013na1m4g8c3rcfw0dwmv2zmia6byg5c2xdx3z5dk90i27s449kx";
   };
   nixpkgs = import nixpkgsTarball { };
 
@@ -18,12 +25,12 @@ with nixpkgs;
 haskell.lib.buildStackProject {
   name = "termonad";
   buildInputs = [
+    cairo
     gnome3.vte
     gobjectIntrospection
     gtk3
     zlib
     # Some of these are probably not actually needed...
-    # cairo
     # gcc
     # glib
     # gnome2.gtk
@@ -36,6 +43,9 @@ haskell.lib.buildStackProject {
     # pkgconfig
     # zlib
   ];
-  ghc = haskell.compiler.ghc843;
-  stack = myStack;
+  ghc = haskell.compiler.ghc802;
+  extraArgs = [
+    "--stack-yaml stack-lts-9.yaml"
+  ];
+  # stack = myStack;
 }
