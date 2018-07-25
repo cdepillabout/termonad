9f4c956eb823618b7232b23c9e9a6ceb04557296
Dennis Gosnell <cdep.illabout@gmail.com>
2018-07-25 11:41:17 +0900

    Start trying to figure out the gtk version.

--- Setup.hs
+++ Setup.hs
@@ -8,7 +8,11 @@
 {-# OPTIONS_GHC -Wall #-}
 module Main (main) where
 
-import Distribution.Simple (UserHooks, defaultMainWithHooks, simpleUserHooks)
+import Distribution.PackageDescription (HookedBuildInfo, cppOptions, emptyBuildInfo)
+import Distribution.Simple (Args, UserHooks, defaultMainWithHooks, preBuild, simpleUserHooks)
+import Distribution.Simple.Program (configureProgram, defaultProgramConfiguration, getDbProgramOutput, pkgConfigProgram)
+import Distribution.Simple.Setup (BuildFlags)
+import Distribution.Verbosity (verbose)
 
 #ifndef MIN_VERSION_cabal_doctest
 #define MIN_VERSION_cabal_doctest(x,y,z) 0
@@ -41,4 +45,36 @@ main = defaultMainWithHooks $ addPkgConfigGtkUserHook simpleUserHooks
 #endif
 
 addPkgConfigGtkUserHook :: UserHooks -> UserHooks
-addPkgConfigGtkUserHook = id
+addPkgConfigGtkUserHook oldUserHooks =
+  oldUserHooks
+    { preBuild = pkgConfigGtkPreBuildHook $ preBuild oldUserHooks
+    }
+
+pkgConfigGtkPreBuildHook :: (Args -> BuildFlags -> IO HookedBuildInfo) -> Args -> BuildFlags -> IO HookedBuildInfo
+pkgConfigGtkPreBuildHook oldFunc args buildFlags = do
+  (maybeOldLibBuildInfo, oldExesBuildInfo) <- oldFunc args buildFlags
+  case maybeOldLibBuildInfo of
+    Just oldLibBuildInfo -> do
+      putStrLn "In Setup.hs, in pkgConfigGtkPreBuildHook, oldLibBuildInfo is not Nothing:"
+      print oldLibBuildInfo
+      putStrLn "\nDon't know how to proceed."
+      error "Build info not Nothing."
+    Nothing -> do
+      pkgDb <-
+        configureProgram verbose pkgConfigProgram defaultProgramConfiguration
+      pkgConfigOutput <-
+        getDbProgramOutput
+          verbose
+          pkgConfigProgram
+          pkgDb
+          ["--modversion", "gtk+-3.0"]
+      print pkgConfigOutput
+      let maybeGtkVersion = readMaybe pkgConfigOutput
+      -- Drop the newline on the end of the pkgConfigOutput.
+      -- This should give us a version number like @3.22.11@.
+      let versionNum = reverse $ drop 1 $ reverse pkgConfigOutput
+      let newLibBuildInfo =
+            emptyBuildInfo
+              { cppOptions = ["-DFOO_BAR_BAZ2"]
+              }
+      pure (Just newLibBuildInfo, oldExesBuildInfo)
