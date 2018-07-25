8f8f6e8c4cf54049f82743838cbc0184f2dd42a8
(cdep)illabout <cdep.illabout@gmail.com>
2018-07-25 20:07:09 +0900

    Update shell.nix to use the latest version of nixpkgs master.

--- shell.nix
+++ shell.nix
@@ -6,18 +6,18 @@ let
     #sha256 = "0kpx4h9p1lhjbn1gsil111swa62hmjs9g93xmsavfiki910s73sh";
 
     # 18.03 (this works)
-    url = "https://github.com/NixOS/nixpkgs/archive/120b013e0c082d58a5712cde0a7371ae8b25a601.tar.gz";
-    sha256 = "0hk4y2vkgm1qadpsm4b0q1vxq889jhxzjx3ragybrlwwg54mzp4f";
+    #url = "https://github.com/NixOS/nixpkgs/archive/120b013e0c082d58a5712cde0a7371ae8b25a601.tar.gz";
+    #sha256 = "0hk4y2vkgm1qadpsm4b0q1vxq889jhxzjx3ragybrlwwg54mzp4f";
 
-    # recent version of nixpkgs as of 2018-07-19.  Doesn't work because gtk3, etc libraries are too old?
-    #url = "https://github.com/NixOS/nixpkgs/archive/d7d31fea7e7eef8ff4495e75be5dcbb37fb215d0.tar.gz";
-    #sha256 = "1ghb1nhgfx3r2rl501r8k0akmfjvnl9pis92if35pawsxgp115kv";
+    # recent version of nixpkgs as of 2018-07-25.
+    url = "https://github.com/NixOS/nixpkgs/archive/4ccaa7de8eb34a0bb140f109a0e88095480118eb.tar.gz";
+    sha256 = "0svqfijh51g62k030dmhsn736pmbnb20baanmapn5wqkn9352nn4";
   };
   nixpkgs = import nixpkgsTarball { };
 
-  fixed-stack-repo = import ./.nix-helpers/fixed-stack.nix { inherit nixpkgs; };
+  # fixed-stack-repo = import ./.nix-helpers/fixed-stack.nix { inherit nixpkgs; };
 
-  myStack = fixed-stack-repo.working.stack;
+  # myStack = fixed-stack-repo.working.stack;
 in
 
 with nixpkgs;
@@ -33,9 +33,9 @@ haskell.lib.buildStackProject {
     gtk3
     zlib
   ];
-  ghc = haskell.compiler.ghc802;
+  ghc = haskell.compiler.ghc843;
   extraArgs = [
-    "--stack-yaml stack-lts-9.yaml"
+    "--stack-yaml stack-lts-12.yaml"
   ];
   # stack = myStack;
 }
