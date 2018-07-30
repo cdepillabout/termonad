daa0e113e18c872d0bd65bf137ba25c0120a4cea
(cdep)illabout <cdep.illabout@gmail.com>
2018-07-30 11:38:59 +0900

    Add a file that helps us run termonad using nix-shell.

---
+++ .nix-helpers/running-termonad.nix
@@ -0,0 +1,16 @@
+
+let
+  nixpkgsTarball = builtins.fetchTarball {
+    # recent version of nixpkgs as of 2018-07-29
+    url = "https://github.com/NixOS/nixpkgs/archive/a2c6dbe370160ffea5537f64dda04489184c5ce1.tar.gz";
+    sha256 = "1x993g9343yv5wyp29i6vskdcc3rl42xipv79nwmmrj8ay2yhh3b";
+  };
+  nixpkgs = import nixpkgsTarball { };
+
+  ghcStuff = nixpkgs.pkgs.haskell.packages.ghc843.ghcWithPackages (pkgs: [
+    (nixpkgs.callPackage ../. { })
+  ]);
+
+in
+
+nixpkgs.runCommand "dummy" { buildInputs = [ ghcStuff ]; } ""
