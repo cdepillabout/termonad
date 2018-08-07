23f09505edef1b91f13e3eff002c3479de0ebec8
(cdep)illabout <cdep.illabout@gmail.com>
2018-08-07 23:33:02 +0900

    Get nix stuff compiling without having to create my own gi-vte derivation.

--- default.nix
+++ default.nix
@@ -8,16 +8,25 @@ let
   };
   nixpkgs = import nixpkgsTarball { };
 
-  vte = nixpkgs.gnome3.vte;
-
-  gtk3 = nixpkgs.gnome3.gtk;
-
-  gi-vte = nixpkgs.pkgs.haskell.packages.${compiler}.callPackage .nix-helpers/gi-vte.nix {
-    inherit gtk3 vte;
+  set-gi-vte-version = _: {
+    version = "2.91.18";
+    sha256 = "0rixrkw0k2vz59y20lsd8zw54n7l069mij0n76dnmah2bjjk1r7w";
   };
+
+  set-gi-gtk-version = _: {
+    version = "3.0.24";
+    sha256 = "14cyj1acxs39avciyzqqb1qa5dr4my8rv3mfwv1kv92wa9a5i97v";
+  };
+
+  allHaskellPackages = nixpkgs.pkgs.haskell.packages.${compiler}.override {
+    overrides = self: super: let lib = nixpkgs.pkgs.haskell.lib; in {
+      gi-gtk = lib.overrideCabal super.gi-gtk set-gi-gtk-version;
+      gi-vte = lib.overrideCabal (lib.addPkgconfigDepend (super.gi-vte.override { vte = nixpkgs.gnome3.vte; }) nixpkgs.gnome3.gtk) set-gi-vte-version;
+    };
+  };
+
 in
 
-nixpkgs.pkgs.haskell.packages.${compiler}.callPackage .nix-helpers/termonad.nix {
-  inherit gi-vte gtk3;
+allHaskellPackages.callPackage .nix-helpers/termonad.nix {
+  inherit (nixpkgs.gnome3) gtk3;
 }
-
