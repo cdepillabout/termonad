9b25ca1e97d969eb84eef871db91364bf3ed2605
(cdep)illabout <cdep.illabout@gmail.com>
2018-08-08 10:56:29 +0900

    Make sure that shell.nix gives us cabal-install.

--- shell.nix
+++ shell.nix
@@ -1,2 +1,14 @@
 { compiler ? "ghc843" }:
-(import ./default.nix { inherit compiler; }).env
+
+let
+  nixpkgsTarball = builtins.fetchTarball {
+    # recent version of nixpkgs as of 2018-07-29
+    url = "https://github.com/NixOS/nixpkgs/archive/a2c6dbe370160ffea5537f64dda04489184c5ce1.tar.gz";
+    sha256 = "1x993g9343yv5wyp29i6vskdcc3rl42xipv79nwmmrj8ay2yhh3b";
+  };
+  nixpkgs = import nixpkgsTarball { };
+in
+
+nixpkgs.lib.overrideDerivation ((import ./default.nix { inherit compiler; }).env) (old: {
+  nativeBuildInputs = old.nativeBuildInputs ++ [ nixpkgs.pkgs.haskell.packages.${compiler}.cabal-install ];
+})
