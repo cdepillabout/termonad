2e6dc4879d5014d2dae340dc0f34b736bb4700bb
Solomon Bothwell <ssbothwell@gmail.com>
2020-06-22 18:39:17 -0700

    Fixes example configs

--- example-config/ExampleGruvboxColourExtension.hs
+++ example-config/ExampleGruvboxColourExtension.hs
@@ -32,11 +32,14 @@ import Termonad.Config.Colour
   , createColour
   , createColourExtension
   , defaultColourConfig
+  , defaultStandardColours
+  , defaultLightColours
   , backgroundColour
   , foregroundColour
   , palette
+  , List8
+  , mkList8
   )
-import Termonad.Config.Vec (Vec((:*), EmptyVec), N8)
 
 -- This is our main 'TMConfig'.  It holds all of the non-colour settings
 -- for Termonad.
@@ -66,29 +69,29 @@ gruvboxDark =
     , palette = ExtendedPalette gruvboxDark1 gruvboxDark2
     }
   where
-    gruvboxDark1 :: Vec N8 (AlphaColour Double)
-    gruvboxDark1 =
-         createColour  40  40  40 -- bg0
-      :* createColour 204  36  29 -- red.1
-      :* createColour 152 151  26 -- green.2
-      :* createColour 215 153  33 -- yellow.3
-      :* createColour  69 133 136 -- blue.4
-      :* createColour 177  98 134 -- purple.5
-      :* createColour 104 157 106 -- aqua.6
-      :* createColour 189 174 147 -- fg3
-      :* EmptyVec
+    gruvboxDark1 :: List8 (AlphaColour Double)
+    gruvboxDark1 = maybe defaultStandardColours id $ mkList8
+      [ createColour  40  40  40 -- bg0
+      , createColour 204  36  29 -- red.1
+      , createColour 152 151  26 -- green.2
+      , createColour 215 153  33 -- yellow.3
+      , createColour  69 133 136 -- blue.4
+      , createColour 177  98 134 -- purple.5
+      , createColour 104 157 106 -- aqua.6
+      , createColour 189 174 147 -- fg3
+      ]
 
-    gruvboxDark2 :: Vec N8 (AlphaColour Double)
-    gruvboxDark2 =
-         createColour 124 111 100 -- bg4
-      :* createColour 251  71  52 -- red.9
-      :* createColour 184 187  38 -- green.10
-      :* createColour 250 189  47 -- yellow.11
-      :* createColour 131 165 152 -- blue.12
-      :* createColour 211 134 155 -- purple.13
-      :* createColour 142 192 124 -- aqua.14
-      :* createColour 235 219 178 -- fg1
-      :* EmptyVec
+    gruvboxDark2 :: List8 (AlphaColour Double)
+    gruvboxDark2 = maybe defaultStandardColours id $ mkList8
+      [ createColour 124 111 100 -- bg4
+      , createColour 251  71  52 -- red.9
+      , createColour 184 187  38 -- green.10
+      , createColour 250 189  47 -- yellow.11
+      , createColour 131 165 152 -- blue.12
+      , createColour 211 134 155 -- purple.13
+      , createColour 142 192 124 -- aqua.14
+      , createColour 235 219 178 -- fg1
+      ]
 
 -- This is our Gruvbox light 'ColourConfig'.  It holds all of our dark-related settings.
 gruvboxLight :: ColourConfig (AlphaColour Double)
@@ -101,29 +104,29 @@ gruvboxLight =
     , palette = ExtendedPalette gruvboxLight1 gruvboxLight2
     }
   where
-    gruvboxLight1 :: Vec N8 (AlphaColour Double)
-    gruvboxLight1 =
-         createColour 251 241 199 -- bg0
-      :* createColour 204  36  29 -- red.1
-      :* createColour 152 151  26 -- green.2
-      :* createColour 215 153  33 -- yellow.3
-      :* createColour  69 133 136 -- blue.4
-      :* createColour 177  98 134 -- purple.5
-      :* createColour 104 157 106 -- aqua.6
-      :* createColour 102  82  84 -- fg3
-      :* EmptyVec
+    gruvboxLight1 :: List8 (AlphaColour Double)
+    gruvboxLight1 = maybe defaultLightColours id $ mkList8
+      [ createColour 251 241 199 -- bg0
+      , createColour 204  36  29 -- red.1
+      , createColour 152 151  26 -- green.2
+      , createColour 215 153  33 -- yellow.3
+      , createColour  69 133 136 -- blue.4
+      , createColour 177  98 134 -- purple.5
+      , createColour 104 157 106 -- aqua.6
+      , createColour 102  82  84 -- fg3
+      ]
 
-    gruvboxLight2 :: Vec N8 (AlphaColour Double)
-    gruvboxLight2 =
-         createColour 168 153 132 -- bg4
-      :* createColour 157   0   6 -- red.9
-      :* createColour 121 116  14 -- green.10
-      :* createColour 181 118  20 -- yellow.11
-      :* createColour   7 102 120 -- blue.12
-      :* createColour 143  63 113 -- purple.13
-      :* createColour  66 123  88 -- aqua.14
-      :* createColour  60  56  54 -- fg1
-      :* EmptyVec
+    gruvboxLight2 :: List8 (AlphaColour Double)
+    gruvboxLight2 = maybe defaultLightColours id $ mkList8
+      [ createColour 168 153 132 -- bg4
+      , createColour 157   0   6 -- red.9
+      , createColour 121 116  14 -- green.10
+      , createColour 181 118  20 -- yellow.11
+      , createColour   7 102 120 -- blue.12
+      , createColour 143  63 113 -- purple.13
+      , createColour  66 123  88 -- aqua.14
+      , createColour  60  56  54 -- fg1
+      ]
 
 
 -- This defines the font for the terminal.
