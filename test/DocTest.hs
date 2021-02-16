425dfb2b72f349c310a5164d13eee71c299cf29c
(cdep)illabout <cdep.illabout@gmail.com>
2021-02-16 22:56:56 +0900

    Try running doctest in a separate process. It appears to no longer crash.

--- test/DocTest.hs
+++ test/DocTest.hs
@@ -2,11 +2,16 @@
 module Main where
 
 import Build_doctests (flags, pkgs, module_sources)
-import Test.DocTest (doctest)
+-- import Test.DocTest (doctest)
+import System.Process (proc, waitForProcess, withCreateProcess)
+import System.Exit (exitWith)
 
 main :: IO ()
 main = do
-  doctest args
+  -- doctest args
+  withCreateProcess (proc "doctest" args) $ \_ _ _ h -> do
+    exitCode <- waitForProcess h
+    exitWith exitCode
   where
     args :: [String]
     args = flags ++ pkgs ++ module_sources
