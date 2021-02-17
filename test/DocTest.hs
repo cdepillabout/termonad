b92fc3deb95de26737fd2f34386eb13a012ab608
(cdep)illabout <cdep.illabout@gmail.com>
2021-02-17 10:05:19 +0900

    wip

--- test/DocTest.hs
+++ test/DocTest.hs
@@ -9,6 +9,7 @@ import System.Exit (exitWith)
 main :: IO ()
 main = do
   -- doctest args
+  -- TESTTEST
   withCreateProcess (proc "doctest" args) $ \_ _ _ h -> do
     exitCode <- waitForProcess h
     exitWith exitCode
