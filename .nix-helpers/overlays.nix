edbbedb5f09551c1fba2be12dbb55f115041a950
Dennis Gosnell <cdep.illabout@gmail.com>
2024-01-07 21:24:47 +0900

    Bump nixpkgs-unstable and default compiler in Nix to ghc94

--- .nix-helpers/overlays.nix
+++ .nix-helpers/overlays.nix
@@ -78,7 +78,7 @@ let
     #
     # Either this, or termonadKnownWorkingHaskellPkgSet can be changed in an overlay
     # if you want to use a different GHC to build Termonad.
-    termonadCompilerVersion = "ghc92";
+    termonadCompilerVersion = "ghc94";
 
     # A Haskell package set where we know the GHC version works to compile
     # Termonad.  This is basically just a shortcut so that other Nix files
